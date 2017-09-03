c581c0cdfc0f5d7bdc4e4c126b7d174ea95fc59c
qioixiy <qioixiy@gmail.com>
2017-09-03 21:27:26 +0800

    sdcv

---
+++ lisp/init-sdcv.el
@@ -0,0 +1,12 @@
+;;; sdcv dictory
+
+;;; stardict-langdao-ec-gb-2.4.2.tar.bz2/stardict-langdao-ce-gb-2.4.2.tar.bz2
+;;; tar jxvf stardict-langdao-ec -C ~/.stardict/dic
+
+(maybe-require-package 'sdcv)
+
+(require 'sdcv)
+
+(provide 'init-sdcv)
+
+;;;
